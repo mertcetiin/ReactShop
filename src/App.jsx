58c0c40f95662cabe94f253f56a172f05897ce2b
Mert Çetin <mrtctn506@gmail.com>
2023-08-30 09:55:21 +0300

    vite and router, formik, yup loaded

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './components/HomePage'
+
+function App() {
+
+
+  return (
+    <div>
+      <Routes>
+        <Route path='' element={<HomePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
