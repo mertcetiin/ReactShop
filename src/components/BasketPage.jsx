435a24286bc56ba562505aaf217078702ee99671
Mert Çetin <mrtctn506@gmail.com>
2023-08-31 22:58:02 +0300

    basketPage made

--- src/components/BasketPage.jsx
+++ src/components/BasketPage.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-
 function BasketPage({ products }) {
-
 
     return (
         <div>
-            <p>Sepetinizde hiç ürün Bulunmamaktadır</p>
-
-            {products.map((item) => (
-                <div key={item.id}>
-                    <h2>{item.name} x {item.count || 0}</h2>
+            {products.some(item => item.count > 0) ? (
+                <div>
+                    {products.map((item) => (
+                        <div key={item.id}>
+                            <h2>{item.name} x {item.count || 0}</h2>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            ) : (
+                <p>Sepetinizde hiç ürün Bulunmamaktadır</p>
+            )}
 
             <Link to='/'>Home Page</Link>
         </div>
