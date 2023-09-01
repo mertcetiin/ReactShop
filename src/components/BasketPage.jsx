c772f87859d5f3dde82ce619821d9462e5ec4544
Mert Çetin <mrtctn506@gmail.com>
2023-09-01 11:27:25 +0300

    basketPage css made

--- src/components/BasketPage.jsx
+++ src/components/BasketPage.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 function BasketPage({ products }) {
 
     return (
-        <div>
+        <div className='basket-div'>
             {products.some(item => item.count > 0) ? (
                 <div>
                     {products.map((item) => (
                         <div key={item.id}>
-                            <h2>{item.name} x {item.count || 0}</h2>
+                            <h2>{item.name} - {item.count || 0}</h2>
                         </div>
                     ))}
                 </div>
             ) : (
-                <p>Sepetinizde hiç ürün Bulunmamaktadır</p>
+                <p className='basket-p'><ShoppingCartIcon /> You have no items in your basket!</p>
             )}
 
-            <Link to='/'>Home Page</Link>
+            <Link to='/' className='basket-link'>Continue Shopping</Link>
         </div>
     )
 }
