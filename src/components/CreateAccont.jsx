e0eca0a3674c874b049b6424dabf528d154c9cc7
Mert Çetin <mrtctn506@gmail.com>
2023-09-04 18:35:40 +0300

    createAccont form made

--- src/components/CreateAccont.jsx
+++ src/components/CreateAccont.jsx
@@ -1,37 +1,63 @@
 import React from 'react'
-import HomePage from './HomePage';
 import { createSchema } from '../Schema';
 import { useFormik } from 'formik';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../styles/CreateAccont.css'
 
 
 function CreateAccont() {
 
+    const onSubmit = async (values, actions) => {
+        await new Promise((resolve) => {
+            setTimeout(resolve, 1000)
+        })
+        actions.resetForm();
+        navigate('/')
+    }
 
-    const { values, errors, handleSubmit, handleChange } = useFormik({
-        onSubmit: () => {
 
-        }
-
+    const { values, errors, handleSubmit, handleChange, isSubmitting } = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirmPassword: '',
+        },
+        validationSchema: createSchema, onSubmit
     })
+
+    const navigate = useNavigate();
 
     return (
         <div className='createForm-div'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='createInput-div'>
                     <label>Email</label>
-                    <input />
+                    <input type='email'
+                        id='email'
+                        onChange={handleChange}
+                        value={values.email}
+                    />
+                    {errors.email && <p className='error'>{errors.email}</p>}
                 </div>
                 <div className='createInput-div'>
                     <label>Password</label>
-                    <input />
+                    <input type='password'
+                        id='password'
+                        onChange={handleChange}
+                        value={values.password}
+                    />
+                    {errors.password && <p className='error'>{errors.password}</p>}
                 </div>
                 <div className='createInput-div'>
                     <label>Confirm Password</label>
-                    <input />
+                    <input type='password'
+                        id='confirmPassword'
+                        onChange={handleChange}
+                        value={values.confirmPassword}
+                    />
+                    {errors.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
                 </div>
-                <Link to='/' className='createLogin-btn'>Create Accont</Link>
+                <button type='submit' className='createLogin-btn' disabled={isSubmitting}>Create Accont</button>
             </form>
 
         </div>
