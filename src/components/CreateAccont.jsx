44192728a3c8241476370cd5f43c33862994bf18
Mert Çetin <mrtctn506@gmail.com>
2023-09-04 18:11:19 +0300

    createAccont basic css made

--- src/components/CreateAccont.jsx
+++ src/components/CreateAccont.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 import HomePage from './HomePage';
 import { createSchema } from '../Schema';
 import { useFormik } from 'formik';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../styles/CreateAccont.css'
 
 
 function CreateAccont() {
 
-    const navigate = useNavigate();
 
     const { values, errors, handleSubmit, handleChange } = useFormik({
         onSubmit: () => {
-            navigate('')
+
         }
 
     })
 
     return (
-        <div className='form-div'>
+        <div className='createForm-div'>
             <form>
-                <div className='input-div'>
+                <div className='createInput-div'>
                     <label>Email</label>
                     <input />
                 </div>
-                <div className='input-div'>
+                <div className='createInput-div'>
                     <label>Password</label>
                     <input />
                 </div>
-                <div className='input-div'>
+                <div className='createInput-div'>
                     <label>Confirm Password</label>
                     <input />
                 </div>
-                <button type='submit' className='create-btn'>Create Accont</button>
+                <Link to='/' className='createLogin-btn'>Create Accont</Link>
             </form>
 
         </div>
